9aa4c732fffa6806261945c1327d75f544cd44e0
Joanna Stanik <asia.stanik752@gmail.com>
2025-11-30 19:41:10 +0100

    Icon + music change

--- Assets/Scripts/DanceSequence.cs
+++ Assets/Scripts/DanceSequence.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "DanceSequence", menuName = "Rhythm/Dance Sequence")]
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CreateAssetMenu(
+    fileName = "NewDanceSequence",
+    menuName = "Rhythm/Dance Sequence")]
 public class DanceSequence : ScriptableObject
 {
+    [Header("Informacje o utworze")]
+    public string songName;
+    public Sprite coverSprite;
+
     [System.Serializable]
     public struct Step
     {
@@ -15,10 +25,166 @@ public class DanceSequence : ScriptableObject
 
     [Header("Okna czasowe trafienia")]
     public float allowedEarly = 0.12f;
-    [Tooltip("Maks. spóŸnienie (s)")]
     public float allowedLate = 0.12f;
 
     [Header("Progi oceny (wartoœci bezwzglêdne)")]
     public float perfectWindow = 0.05f;
     public float goodWindow = 0.10f;
 }
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(DanceSequence))]
+public class DanceSequenceEditor : Editor
+{
+    SerializedProperty songNameProp;
+    SerializedProperty coverSpriteProp;
+    SerializedProperty stepsProp;
+    SerializedProperty allowedEarlyProp;
+    SerializedProperty allowedLateProp;
+    SerializedProperty perfectWindowProp;
+    SerializedProperty goodWindowProp;
+
+    void OnEnable()
+    {
+        songNameProp = serializedObject.FindProperty("songName");
+        coverSpriteProp = serializedObject.FindProperty("coverSprite");
+        stepsProp = serializedObject.FindProperty("steps");
+        allowedEarlyProp = serializedObject.FindProperty("allowedEarly");
+        allowedLateProp = serializedObject.FindProperty("allowedLate");
+        perfectWindowProp = serializedObject.FindProperty("perfectWindow");
+        goodWindowProp = serializedObject.FindProperty("goodWindow");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.LabelField("Informacje o utworze", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(songNameProp, new GUIContent("Nazwa piosenki"));
+        EditorGUILayout.PropertyField(coverSpriteProp, new GUIContent("Ok³adka"));
+
+        var sprite = coverSpriteProp.objectReferenceValue as Sprite;
+        if (sprite != null)
+        {
+            float size = 128f;
+            Rect rect = GUILayoutUtility.GetRect(size, size, GUILayout.ExpandWidth(false));
+            EditorGUI.DrawPreviewTexture(rect, sprite.texture, null, ScaleMode.ScaleToFit);
+        }
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Kroki sekwencji", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(stepsProp, true);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Parametry czasowe", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(allowedEarlyProp, new GUIContent("Allowed Early"));
+        EditorGUILayout.PropertyField(allowedLateProp, new GUIContent("Allowed Late"));
+        EditorGUILayout.PropertyField(perfectWindowProp, new GUIContent("Perfect Window"));
+        EditorGUILayout.PropertyField(goodWindowProp, new GUIContent("Good Window"));
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+
+public class DanceSequenceEditorWindow : EditorWindow
+{
+    private DanceSequence sequence;
+    private SerializedObject serializedSequence;
+
+    SerializedProperty songNameProp;
+    SerializedProperty coverSpriteProp;
+    SerializedProperty stepsProp;
+    SerializedProperty allowedEarlyProp;
+    SerializedProperty allowedLateProp;
+    SerializedProperty perfectWindowProp;
+    SerializedProperty goodWindowProp;
+
+    [MenuItem("Window/Rhythm/Dance Sequence Editor")]
+    public static void OpenWindow()
+    {
+        var window = GetWindow<DanceSequenceEditorWindow>("Dance Sequence");
+        window.Show();
+    }
+
+    void SetSequence(DanceSequence seq)
+    {
+        sequence = seq;
+        if (sequence != null)
+        {
+            serializedSequence = new SerializedObject(sequence);
+
+            songNameProp = serializedSequence.FindProperty("songName");
+            coverSpriteProp = serializedSequence.FindProperty("coverSprite");
+            stepsProp = serializedSequence.FindProperty("steps");
+            allowedEarlyProp = serializedSequence.FindProperty("allowedEarly");
+            allowedLateProp = serializedSequence.FindProperty("allowedLate");
+            perfectWindowProp = serializedSequence.FindProperty("perfectWindow");
+            goodWindowProp = serializedSequence.FindProperty("goodWindow");
+        }
+
+        Repaint();
+    }
+
+    void OnSelectionChange()
+    {
+        if (Selection.activeObject is DanceSequence ds)
+        {
+            SetSequence(ds);
+        }
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.LabelField("Dance Sequence Editor", EditorStyles.boldLabel);
+        EditorGUILayout.Space();
+
+        var newSeq = (DanceSequence)EditorGUILayout.ObjectField(
+            "Sekwencja",
+            sequence,
+            typeof(DanceSequence),
+            false);
+
+        if (newSeq != sequence)
+            SetSequence(newSeq);
+
+        if (sequence == null)
+        {
+            EditorGUILayout.HelpBox(
+                "Wybierz asset typu DanceSequence",
+                MessageType.Info);
+            return;
+        }
+
+        serializedSequence.Update();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Informacje o utworze", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(songNameProp, new GUIContent("Nazwa piosenki"));
+        EditorGUILayout.PropertyField(coverSpriteProp, new GUIContent("Ok³adka"));
+
+        var sprite = coverSpriteProp.objectReferenceValue as Sprite;
+        if (sprite != null)
+        {
+            float size = 128f;
+            Rect rect = GUILayoutUtility.GetRect(size, size, GUILayout.ExpandWidth(false));
+            EditorGUI.DrawPreviewTexture(rect, sprite.texture, null, ScaleMode.ScaleToFit);
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Kroki sekwencji", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(stepsProp, true);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Parametry czasowe", EditorStyles.boldLabel);
+        EditorGUILayout.PropertyField(allowedEarlyProp, new GUIContent("Allowed Early"));
+        EditorGUILayout.PropertyField(allowedLateProp, new GUIContent("Allowed Late"));
+        EditorGUILayout.PropertyField(perfectWindowProp, new GUIContent("Perfect Window"));
+        EditorGUILayout.PropertyField(goodWindowProp, new GUIContent("Good Window"));
+
+        serializedSequence.ApplyModifiedProperties();
+    }
+}
+#endif
--- Assets/Scripts/PromptIcon.cs
+++ Assets/Scripts/PromptIcon.cs
@@ -3,34 +3,121 @@ using UnityEngine.UI;
 
 public class PromptIcon : MonoBehaviour
 {
+    [Header("UI")]
     public Text label;            // Tekst z nazw¹ klawisza (np. "A")
     public Image background;      // Opcjonalnie t³o do podœwietlania
+    public Image iconImage;
+
+    [Header("kolory")]
     public Color idleColor = Color.white;
     public Color hitColor = Color.green;
     public Color missColor = Color.red;
 
+    [Header("Klawisze")]
+    public Sprite spriteW;
+    public Sprite spriteA;
+    public Sprite spriteS;
+    public Sprite spriteD;
+    public Sprite defaultSprite;
+
+    private KeyCode _key;
+
     public void SetKey(KeyCode key)
     {
-        if (label) label.text = key.ToString();
+        _key = key;
+
+        if (label) 
+            label.text = key.ToString();
+
+        if (iconImage)
+            iconImage.sprite = GetSpriteForKey(key);
+
+
         SetIdle();
+    }
+
+    private Sprite GetSpriteForKey(KeyCode key)
+    {
+        switch (key)
+        {
+            case KeyCode.W: return spriteW;
+            case KeyCode.A: return spriteA;
+            case KeyCode.S: return spriteS;
+            case KeyCode.D: return spriteD;
+            default: return defaultSprite;
+        }
     }
 
     public void SetIdle()
     {
-        if (label) label.color = idleColor;
-        if (background) background.color = new Color(background.color.r, background.color.g, background.color.b, 0.35f);
+        if (label)
+        {
+            var c = idleColor;
+            c.a = label.color.a;
+            label.color = c;
+        }
+
+        if (background)
+        {
+            var c = idleColor;
+            c.a = background.color.a;
+            background.color = c;
+        }
+
+        if (iconImage)
+        {
+            var c = Color.white;
+            c.a = iconImage.color.a;
+            iconImage.color = c;
+        }
     }
 
     public void SetHit()
     {
-        if (label) label.color = hitColor;
-        if (background) background.color = new Color(hitColor.r, hitColor.g, hitColor.b, 0.35f);
+        if (label)
+        {
+            var c = hitColor;
+            c.a = label.color.a;
+            label.color = c;
+        }
+
+        if (background)
+        {
+            var c = hitColor;
+            c.a = background.color.a;
+            background.color = c;
+        }
+
+        if (iconImage)
+        {
+            var c = hitColor;
+            c.a = iconImage.color.a;
+            iconImage.color = c;
+        }
     }
 
     public void SetMiss()
     {
-        if (label) label.color = missColor;
-        if (background) background.color = new Color(missColor.r, missColor.g, missColor.b, 0.35f);
+        if (label)
+        {
+            var c = missColor;
+            c.a = label.color.a;
+            label.color = c;
+        }
+
+        if (background)
+        {
+            var c = missColor;
+            c.a = background.color.a;
+            background.color = c;
+        }
+
+        if (iconImage)
+        {
+            var c = missColor;
+            c.a = iconImage.color.a;
+            iconImage.color = c;
+        }
     }
 
     public void SetAlpha(float a)
@@ -42,6 +129,11 @@ public class PromptIcon : MonoBehaviour
         if (background)
         {
             var c = background.color; c.a = a * 0.35f; background.color = c;
+        }
+
+        if (iconImage)
+        {
+            var c = iconImage.color; c.a = a; iconImage.color = c;
         }
     }
 }
